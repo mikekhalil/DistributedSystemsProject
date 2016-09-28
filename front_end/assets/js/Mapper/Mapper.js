af126532592eb5a4f608c0933a3b93464debd0d9
pheldring <pheldrin@purdue.edu>
2016-09-27 20:11:02 -0400

    made mapper pretty

--- front_end/assets/js/Mapper/Mapper.js
+++ front_end/assets/js/Mapper/Mapper.js
@@ -1,4 +1,4 @@
-// test
+/* test
 var records = [ {"key":"0", "value":"Paul"}, {"key":"1", "value":"Alex"}, {"key":"2", "value":"Paul"}, {"key":"3", "value":"Dave"} ];
 
 function wordCount( key, value ) {
@@ -7,36 +7,39 @@ function wordCount( key, value ) {
     list.push({key : value, value : count});
     return list;
 }
+*/
 
-var shuffledList = new Array();
-var pairs = {};
+function Mapper( mapFunc ) {
+    var shuffledList = new Array(); /* shuffled and sorted list	   */
+    var pairs = {};		    /* array returned from mapFunc */
 
-// might have to do binary search
-function indexOfVal( str ) {
-    for (var i in shuffledList) {
-	if (shuffledList[i].key === str) {
-	    return i;
+    function indexOfVal( str ) {
+	for (var i in shuffledList) {
+	    if (shuffledList[i].key === str) {
+		return i;
+	    }
+	}
+	return -1;
+    }
+
+    for (var i in records) {
+	// call map function
+	pairs =  mapFunc( records[i].key, records[i].value );
+	for (var j in pairs) {
+	    var index = indexOfVal(pairs[j].key);
+	    if ( index == -1 ) {
+		// key doesn't exit, create an array and append value to array
+		var tmp = pairs[j].value;
+		pairs[j].value = new Array();
+		pairs[j].value.push(tmp);
+		shuffledList.push(pairs[j]);
+	    } else {
+		// key exists, append value to array
+		shuffledList[index].value.push(pairs[j].value);
+	    }
 	}
     }
-    return -1;
+    shuffledList.sort(function(a, b) { return a.key.localeCompare(b.key);});
+    console.log(shuffledList);
+    return shuffledList;
 }
-
-for (var i in records) {
-    // call map function
-    pairs =  wordCount( records[i].key, records[i].value );
-    for (var j in pairs) {
-	var index = indexOfVal(pairs[j].key);
-	if ( index == -1 ) {
-	    // key doesn't exit, create an array and append value to array
-	    var tmp = pairs[j].value;
-	    pairs[j].value = new Array();
-	    pairs[j].value.push(tmp);
-	    shuffledList.push(pairs[j]);
-	} else {
-	    // key exists, append value to array
-	    shuffledList[index].value.push(pairs[j].value);
-	}
-    }
-}
-shuffledList.sort(function(a, b) { return a.key.localeCompare(b.key);});
-console.log(shuffledList);

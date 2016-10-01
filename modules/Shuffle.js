9690fc04d28834f748ac437450fcc4c0df02ff80
pheldring <pheldrin@purdue.edu>
2016-10-01 01:35:00 -0400

    sorted keys in shuffle

--- modules/Shuffle.js
+++ modules/Shuffle.js
@@ -3,25 +3,35 @@
  * Shuffles and sorts each key, value pair
  * Written by Paul Heldring
  */
-function MainShuffle( reducedFiles ) {
+var func = function MainShuffle( reducedFiles ) {
     var fs = require('fs');
     var encoding = 'utf8';
     var shuffledData = {};
+    var orderedData = {};
 
     for ( i in reducedFiles ) {
 	var filePath = reducedFiles[i];
-	var data = fs.readFileSync(filePath, encoding) 
+	var data = fs.readFileSync(filePath, encoding);
 	var keyVal = JSON.parse(data);
 	for ( key in keyVal ) {
 	    if ( shuffledData.hasOwnProperty(key) ) {
+		// key exists, append value to array
 		shuffledData[key].push(keyVal[key]);
 	    } else {
+		// key does not exist, create k,v pair
 		var val = [];
 		val.push(keyVal[key]);
 		shuffledData[key] = val;
 	    }
 	}
     }
-    // still need to sort keys... have to find a good way to do this
-    return shuffledData;
+    // Sort keys, kind of inefficient because read data is already sorted
+    Object.keys(shuffledData).sort().forEach(function(key) {
+	  orderedData[key] = shuffledData[key];
+    });
+    return orderedData;
+}
+
+module.exports = {
+    mainShuffle : func
 }

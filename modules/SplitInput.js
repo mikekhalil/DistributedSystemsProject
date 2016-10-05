a1400efa2fdc42b3d82b524a1b2ae8327fc73fd5
mkhalil <mkhalil@purdue.edu>
2016-10-04 21:35:45 -0400

    Input Split is working!

--- modules/SplitInput.js
+++ modules/SplitInput.js
@@ -2,11 +2,11 @@
  * Returns an array containing the file paths of each chunk.
  * Written by Paul Heldring
  */
+const path = require('path');
 
-var func = function SplitInput( file ) {
+var func = function SplitInput( file , splitDir) {
 
     var fs = require('fs'); 
-    var splitDir = '/splits/';	/* dir where splits are stored. */
     var encoding = 'utf8';	/* character encoding		*/
     var j = 0;			/* count num chars in split	*/
     var split = '';		/* split string			*/
@@ -15,14 +15,13 @@ var func = function SplitInput( file ) {
     var splitCount = 0;		/* number of splits		*/
     var filepath = '';		/* full path of split		*/
 
-    if (!fs.existsSync(__dirname + splitDir)) {
-		fs.mkdirSync(__dirname + splitDir);
+    if (!fs.existsSync(splitDir)) {
+		fs.mkdirSync(splitDir);
     }
 
-    fs.readFile( file, encoding, function (err, data) {
-	if (err) {
-	    return console.log(err);
-	}
+	console.log(file);
+
+    var data = fs.readFileSync( file, encoding);
 	
 	for (var i = 0; i < data.length; i++) {
 	    // Reached max split size or end of file 
@@ -31,25 +30,21 @@ var func = function SplitInput( file ) {
 		    split += data[i++];
 		    j++;
 		}
-		split += data[i];
-		filepath = __dirname + splitDir + splitCount + '.txt';
-		// Write split to split directory
-		fs.writeFile(filepath, split, encoding, function(err) {
-		    if (err) {
-			return console.log(err);
-		    }
-		});
-		splits[splitCount++] = filepath;
-		split = '';
-		j = 0;
+			split += data[i];
+			filepath = path.join(splitDir, splitCount + '.txt');
+			
+			// Write split to split directory
+			fs.writeFileSync(filepath, split, encoding);
+			splits[splitCount++] = filepath;
+			split = '';
+			j = 0;
 	    }
 	    // Append characters to the split
 	    else {
-		split += data[i];	
-		j++;
+			split += data[i];	
+			j++;
 	    }
 	}
-    });
     
     return splits;
 }

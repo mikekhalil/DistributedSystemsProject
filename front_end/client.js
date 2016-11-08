5d30bf0495933622f232f6ef050ff7f9a36b07d3
mkhalil <mkhalil@purdue.edu>
2016-11-07 23:16:09 -0500

    why webpack tho

--- front_end/client.js
+++ front_end/client.js
@@ -1,6 +1,43 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import ReactStormpath, { Router, AuthenticatedRoute, LoginLink } from 'react-stormpath';
+import { Router } from 'react-stormpath';
+import { Route, browserHistory } from 'react-router';
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IndexRoute, Route, browserHistory } from 'react-router';
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
+import { ChangePasswordPage, MasterPage, IndexPage, LoginPage, RegisterPage, ResetPasswordPage, VerifyEmailPage, ProfilePage } from './pages';
+
+
 
 const app = document.getElementById('app');
-ReactDOM.render(<App />, app);
+
+ReactStormpath.init({
+    // See docs for available configuration options.
+});
+
+
+//ReactDOM.render(<App />, app);
+
+
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <HomeRoute path='/' component={MasterPage}>
+      <IndexRoute component={IndexPage} />
+      <LoginRoute path='/login' component={LoginPage} />
+      <Route path='/verify' component={VerifyEmailPage} />
+      <Route path='/register' component={RegisterPage} />
+      <Route path='/change' component={ChangePasswordPage} />
+      <Route path='/forgot' component={ResetPasswordPage} />
+      <AuthenticatedRoute>
+        <Route path='/profile' component={ProfilePage} />
+      </AuthenticatedRoute>
+    </HomeRoute>
+  </Router>,
+  app
+);

904bf32eaa22c4c64f5513577eb91efeb53e2ac0
mkhalil <mkhalil@purdue.edu>
2016-12-01 19:29:23 -0500

    Implemented Hacky way to determine when all files have been uploaded

--- Manager.js
+++ Manager.js
@@ -16,12 +16,21 @@ var Job = (__dirname + '/modules/Job');
 
 mongoose.connect(config.mongodb.url);
 
+
+//please replace hacky man
+var hacky = {};
+
 //TODO: set up a method that allows new clients that just connected to start comoputing all of the jobs that thare are part of
-var isInitialized = function(doc) {
-    
-    return doc.data!= null && doc.map!= null && doc.reduce!= null;
+var isInitialized = function(job_id) {;    
+    return hacky[job_id] == 3;
 }
 
+var createJob = function(id) {
+   //TODO: Update Group Manager - propogate change to Server / Reducer
+   
+}
+
+var counter = 0;
 //use client table and job table to assign jobs based off availability
 socket.on('UploadedFile', function(file) {
     //update the particular job
@@ -32,34 +41,47 @@ socket.on('UploadedFile', function(file) {
         if( file.type === config.REDUCE || file.type === config.MAP ){
             var fileData = fs.readFileSync(file.data, "utf8");
             doc[file.type] = fileData;
-            console.log(doc);
-
-            doc.save();
+            console.log('map or reduce');
+            doc.save(function(err,doc) { 
+                if(hacky[file.job_id]){
+                    hacky[file.job_id]++;
+                    if(isInitialized(file.job_id)){
+                        createJob(file.job_id);
+                    }
+                }
+                else{
+                    hacky[file.job_id] = 1;
+                }
+            });
         }
         else {
             //Data file, Create Input Splits
             var groupDir = path.join(__dirname,config.multer.path,file.group_id);
             splitter.splitInput(file.data,groupDir,file.job_id,function(inputSplits) {
                 doc.data = file.data;
-                //doc.splits = inputSplits;
                 var index = 0;
                 Object.keys(inputSplits).forEach(function(key){
                     doc.splits.set(index++, inputSplits[key]);
                 });
-                doc.markModified('data');
-                doc.markModified('splits');
-                doc.save();
-                console.log('saved job');
+                doc.save(function(err,doc) {
+                        if(hacky[file.job_id]){
+                        hacky[file.job_id]++;
+                        if(isInitialized(file.job_id)){
+                            getJob(file.job_id);
+                        }
+                    }
+                    else{
+                        hacky[file.job_id] = 0;
+                    }
+                });
+                //console.log('splits + data');
+                //console.log('is initalized : ' + isInitialized(doc));
             });
         }
-        if(isInitialized(doc)){
-            consol.log('job is ready to go');
-            var job = new Job(file.job_id, file.group_id, messenger,config.status.INCOMPLETE,doc.map,doc.reduce,doc.splits);
-            //TODO add to group manager (how to get live updates of group manager?)
-        }
-
     });
-   
+
+
+ 
 });
 
 messenger.inchannel.subscribe("SystemReset" , function(msg) {
@@ -72,4 +94,4 @@ messenger.inchannel.subscribe("Results", function(msg) {
 
 messenger.inchannel.subscribe(config.topics.CLIENT_TABLE_UPDATE, function(msg) {
     //TODO HOW DO WE UPDATE GroupManager here
-})
+});

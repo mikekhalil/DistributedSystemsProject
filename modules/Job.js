5ad262aa526a703e6816290cacc0857547c0cf56
sameetmo <mohanty0@purdue.edu>
2016-12-10 18:28:39 -0500

    removing vhat

--- modules/Job.js
+++ modules/Job.js
@@ -67,7 +67,7 @@ class Job {
     resultHandler(res) {
         //console.log(res);
         //TODO: Add a timeout featue - if job has been active for more tha X seconds, update it to not active - assign job to another node
-        console.log("vhat"); 
+        //console.log("vhat"); 
         var sockid = res.sockid;
         var completedTask = this.tasks[res.inputSplit];
         this.tasks[completedTask.split].setStatus(config.status.COMPLETE);
